6a80b80d0d416d4300d83337b5b69e8edcb6ff42
ebenezer.franklin <ebenezer.franklin@llamasoft.com>
2019-03-11 11:37:20 +0000

    namespace changes

--- source/repos/UnitTestProject3/UnitTestProject2/Properties/AssemblyInfo.cs
+++ source/repos/UnitTestProject3/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyTitle("Specflow_POC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyProduct("Specflow_POC")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
